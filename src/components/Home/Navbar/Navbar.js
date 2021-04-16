d827722afeb583cc4bf716770b909b884d7baaa0
umar <unzippinglinux@gmail.com>
2021-04-16 19:36:12 +0600

    navbar added

--- src/components/Home/Navbar/Navbar.js
+++ src/components/Home/Navbar/Navbar.js
@@ -1,7 +1,57 @@
 import React from "react";
 
 const Navbar = () => {
-  return <nav></nav>;
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light">
+      <div className="container-fluid">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li className="nav-item ms-5">
+              <a className="nav-link active" aria-current="page" href="#">
+                Home
+              </a>
+            </li>
+            <li className="nav-item ms-5">
+              <a className="nav-link" href="#">
+                About
+              </a>
+            </li>
+            <li className="nav-item ms-5">
+              <a className="nav-link" href="#">
+                Dental Services
+              </a>
+            </li>
+            <li className="nav-item ms-5">
+              <a className="nav-link" href="#">
+                Reviews
+              </a>
+            </li>
+            <li className="nav-item ms-5">
+              <a className="nav-link" href="#">
+                Blog
+              </a>
+            </li>
+            <li className="nav-item ms-5">
+              <a className="nav-link" href="#">
+                Contact Us
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
