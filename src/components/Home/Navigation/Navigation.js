a7d97f798beabac82a9520a25bba9e49b88441ae
umar <unzippinglinux@gmail.com>
2021-05-09 04:54:51 +0600

    fixed responsive nav

--- src/components/Home/Navigation/Navigation.js
+++ src/components/Home/Navigation/Navigation.js
@@ -4,29 +4,41 @@ import { Navbar, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 const Navigation = () => {
+  const navAlignOnMob = "mx-auto";
   return (
     <Navbar
-      style={{
-        margin: "0 1.5rem",
-      }}
-      bg="light"
+      className="justify-content-between mx-3"
+      bg="white"
+      variant="light"
       expand="lg"
     >
       <Navbar.Brand href="#home">Cleaner's World</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ms-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#services">Our Services</Nav.Link>
-          <Nav.Link href="#reviews">Reviews</Nav.Link>
-          <Nav.Link>
+          <Nav.Link className={navAlignOnMob} href="#home">
+            Home
+          </Nav.Link>
+          <Nav.Link className={navAlignOnMob} href="#services">
+            Our Services
+          </Nav.Link>
+          <Nav.Link className={navAlignOnMob} href="#reviews">
+            Reviews
+          </Nav.Link>
+          <Nav.Link className={navAlignOnMob}>
             <Link className="navAnchors" to="/admin">
               Admin
             </Link>
           </Nav.Link>
-          <Nav.Link href="#dashboard">Dashboard</Nav.Link>
-          <Nav.Link href="#about">About</Nav.Link>
-          <Nav.Link href="#contact">Contact Us</Nav.Link>
+          <Nav.Link className={navAlignOnMob} href="#dashboard">
+            Dashboard
+          </Nav.Link>
+          <Nav.Link className={navAlignOnMob} href="#about">
+            About
+          </Nav.Link>
+          <Nav.Link className={navAlignOnMob} href="#contact">
+            Contact Us
+          </Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
