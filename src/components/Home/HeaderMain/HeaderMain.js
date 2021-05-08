c5500f1976cc5179b95f3e441292fd1c21dda81a
umar <unzippinglinux@gmail.com>
2021-05-09 01:18:15 +0600

    fixed header

--- src/components/Home/HeaderMain/HeaderMain.js
+++ src/components/Home/HeaderMain/HeaderMain.js
@@ -5,47 +5,51 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const HeaderMain = () => {
   return (
-    <div className="w-75 mx-auto">
-      <main
-        style={{ height: "32rem" }}
-        className="row d-flex align-items-center"
-      >
-        <div className="col-md-4 offset-md-1">
-          <h1 style={{ color: "#3a4256", fontSize: "2.45rem" }}>
-            Your Clean Workspace
-            <br />
-            Our Promise!
-          </h1>
-          <p className="text-secondary">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia at
-            incidunt asperiores fugiat iure dolorem.
-          </p>
-          <button
-            style={{
-              background: "#b2efe4",
-              color: "#3a4256",
-              fontSize: "1.2rem",
-            }}
-            className="btn"
-          >
-            HIRE US NOW{"  "}
-            <FontAwesomeIcon icon={faArrowRight}></FontAwesomeIcon>
-          </button>
-        </div>
-        <div className="col-md-6">
-          <img
-            style={{
-              borderRadius: "0.8rem",
-              width: "80%",
-              marginLeft: "5rem",
-            }}
-            src={mainImg1}
-            alt=""
-            className="img-fluid"
-          />
-        </div>
-      </main>
-    </div>
+    <main
+      style={{ height: "32rem" }}
+      className="row d-flex align-items-center justify-content-center"
+    >
+      {/* action message info start */}
+      <div className="col-md-4 text-center ">
+        <h1 style={{ color: "#3a4256", fontSize: "2.45rem" }}>
+          Your Clean Workspace
+          <br />
+          Our Promise!
+        </h1>
+        <p className="text-secondary text-justify">
+          We provide world class cleaning services. You show the space and leave
+          the rest to us.
+        </p>
+        <a
+          href="#services"
+          style={{
+            background: "#b2efe4",
+            color: "#3a4256",
+            fontSize: "1.2rem",
+          }}
+          className="btn"
+        >
+          HIRE US NOW{"  "}
+          <FontAwesomeIcon icon={faArrowRight}></FontAwesomeIcon>
+        </a>
+      </div>
+      {/* action message info end */}
+
+      {/* main Img info start */}
+
+      <div className="col-md-6 d-flex justify-content-center align-items-center ">
+        <img
+          style={{
+            borderRadius: "0.8rem",
+            width: "80%",
+          }}
+          src={mainImg1}
+          alt=""
+          className="img-fluid"
+        />
+      </div>
+      {/* main Img info start */}
+    </main>
   );
 };
 
