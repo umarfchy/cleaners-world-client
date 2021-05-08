5906b066b27bffaec4b2aebd3ae3c0c5c4aaada0
umar <unzippinglinux@gmail.com>
2021-05-09 01:58:05 +0600

    corporate client ui fixed

--- src/components/Home/CorporateClients/CorporateClients.js
+++ src/components/Home/CorporateClients/CorporateClients.js
@@ -11,22 +11,38 @@ import beximco from "./../../../Images/corporateClients/beximco.png";
 const clientList = [ibbl, nagad, pathao, rokomari, uniliver, alpha, beximco];
 
 const CorporateClients = () => {
+  const renderCorpClientCard = (imagePar, colSpreadSize) => {
+    const classDef = `d-flex align-items-center justify-content-center ${colSpreadSize}`;
+    return (
+      <div
+        style={{
+          border: "0.1rem solid white",
+          boxShadow: "rgba(0, 0, 0, 0.3) 0px 5px 15px",
+          borderRadius: ".8rem",
+          height: "10rem",
+          width: "10rem",
+          margin: ".5rem",
+        }}
+        className={classDef}
+      >
+        <img className="img-fluid w-75" src={imagePar} alt="" />
+      </div>
+    );
+  };
   return (
     <div className="mt-3 mb-5 mx-auto w-75">
-      <h3 style={{ textAlign: "center" }}>
-        Our <span style={{ color: "blue" }}>Corporate Clients</span>
-      </h3>
+      <h3 className="text-center mb-5">Our Corporate Clients</h3>
       <div className="row d-flex justify-content-center">
         <div className="row d-flex justify-content-center">
-          <img src={clientList[0]} alt="" className="col-md-3 corporateImg" />
-          <img src={clientList[1]} alt="" className="col-md-3 corporateImg" />
-          <img src={clientList[2]} alt="" className="col-md-3 corporateImg" />
-          <img src={clientList[3]} alt="" className="col-md-3 corporateImg" />
+          {renderCorpClientCard(clientList[0], "col-md-3")}
+          {renderCorpClientCard(clientList[1], "col-md-3")}
+          {renderCorpClientCard(clientList[2], "col-md-3")}
+          {renderCorpClientCard(clientList[3], "col-md-3")}
         </div>
         <div className="row d-flex justify-content-center">
-          <img src={clientList[5]} alt="" className="col-md-4 corporateImg" />
-          <img src={clientList[6]} alt="" className="col-md-4 corporateImg" />
-          <img src={clientList[4]} alt="" className="col-md-4 corporateImg" />
+          {renderCorpClientCard(clientList[4], "col-md-4")}
+          {renderCorpClientCard(clientList[5], "col-md-4")}
+          {renderCorpClientCard(clientList[6], "col-md-4")}
         </div>
       </div>
     </div>
